d6393b1ba70d2ae3f1c80094d293d2b5c27fe885
ulises39 <ulises.marmolejo39@gmail.com>
2024-01-20 11:21:38 -0600

    End of section 2

---
+++ API/Data/Migrations/20230218223523_ProductsUpdate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductsUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantityStock",
+                table: "Products",
+                newName: "QuantityInStock");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantityInStock",
+                table: "Products",
+                newName: "QuantityStock");
+        }
+    }
+}
